10fab78387cc506e9bb693574ee4ebf0b309ad6a
Kurt Raschke <kurt@kurtraschke.com>
2023-07-13 12:47:49 -0400

    Update pg2parquet.rb

--- Formula/pg2parquet.rb
+++ Formula/pg2parquet.rb
@@ -2,7 +2,7 @@ class Pg2parquet < Formula
   desc "Export PostgreSQL table or query into Parquet file"
   homepage "https://github.com/exyi/pg2parquet"
   license "Apache-2.0"
-  head "https://github.com/exyi/pg2parquet.git", revision: "745323955adc80240ba154756f3e0d1b533f834c"
+  head "https://github.com/exyi/pg2parquet.git", revision: "da1f8e53786fa816d9374e7bab18e5a6a0926186"
 
   depends_on "rust" => :build
 
@@ -10,7 +10,6 @@ class Pg2parquet < Formula
     # ENV.deparallelize  # if your formula fails when building in parallel
     cd "cli" do
       ENV["RUSTFLAGS"] = "-C target-cpu=native"
-      system "cargo", "update", "-p", "geo-types", "--precise", "0.7.8"
       system "cargo", "install", *std_cargo_args
       bin.install "target/release/pg2parquet" => "pg2parquet"
     end
